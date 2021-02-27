8f499afa16abd4fece8cdfc46b7746bc6cddb616
dalopod <dalopod@gmail.com>
2021-02-26 20:24:06 -0800

    near final prototype cool

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,5 @@
 var myFullpage = new fullpage('#fullpage', {
         navigation: true,
-        navigationPosition: 'right'
+        navigationPosition: 'right',
+        navigationTooltips: ['Intro1', 'Intro2', 'Intro3', 'Title', 'Causes1', 'Causes2', 'Effects', 'Map', 'Country1']
     });
