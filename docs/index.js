f4bd7b47907f04feec99fe888672f149aadcdbfa
dalopod <dalopod@gmail.com>
2021-03-15 18:51:38 -0700

    SO CLOSE

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,6 @@
 var myFullpage = new fullpage('#fullpage', {
         navigation: true,
         navigationPosition: 'right',
-        navigationTooltips: ['Intro', 'Title', 'Causes', 'Wildfires', 'Effects', 'Carbon', 'Map', 'Indonesia', 'Biodiversity', 'Bornean', 'Sumatran', 'Brazil', 'Biodiversity', 'Giant Otter', 'Stop Deforestation', 'Methods', 'Timer', 'Credits', 'About'],
+        navigationTooltips: ['Intro', 'Title', 'Causes', 'Wildfires', 'Effects', 'Carbon', 'Map', 'Indonesia', 'Biodiversity', 'Bornean', 'Sumatran', 'Brazil', 'Biodiversity', 'Giant Otter', 'Stop Deforestation', 'Methods', 'Timer', 'References', 'Team Members'],
         showActiveTooltip: true
     });
